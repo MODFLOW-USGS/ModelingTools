7438d5cfe28cbfcd4530aa3e517acd83d720265f
Richard B. Winston <rbwinst@usgs.gov>
2022-01-27 02:25:56 -0500

    Updated Online Guide to MODFLOW.

--- ModelMuse/Documentation5/ModflowGuide/GenerateHelp.bat
+++ ModelMuse/Documentation5/ModflowGuide/GenerateHelp.bat
@@ -30,9 +30,9 @@ rem "C:\Program Files (x86)\EC Software\HelpAndManual5\HELPMAN.EXE" "D:\Colab\GW
 rem "C:\Program Files\EC Software\HelpAndManual5\HELPMAN.EXE" "C:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowGuide.hmxp" GenerateHelpIni.ini
 rem
 rem modify gnerated webhelp output
-"C:\Projects\GoogleAnalytics\Win32\Debug\GoogleAnalytics.exe" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\Modflow2000Guide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowAllGuide"
+"C:\Projects\GoogleAnalytics\Win32\Debug\GoogleAnalytics.exe" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\Modflow2000Guide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowGuide"
 "C:\Projects\OgwAnalytics\Win32\Debug\OgwAnalytics.exe" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\Modflow2005Guide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowNwtGuide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowLgrGuide"
-"C:\Projects\HttpReplace\Win32\Debug\ReplaceHttp.exe" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\Modflow2000Guide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowAllGuide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowNwtGuide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowOwhmGuide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowLgrGuide"
+"C:\Projects\HttpReplace\Win32\Debug\ReplaceHttp.exe" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\Modflow2000Guide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowGuide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowNwtGuide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowOwhmGuide" "C:\ModelingTools\ModelMuse\Documentation5\ModflowGuide\ModflowLgrGuide"
 rem "C:\Users\rbwinst-pr\Documents\RAD Studio\Projects\GoogleAnalytics\Win32\Release\GoogleAnalytics.exe" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\Modflow2000Guide" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowAllGuide"
 rem "C:\Users\rbwinst-pr\Documents\RAD Studio\Projects\OgwAnalytics\Win32\Release\OgwAnalytics.exe" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\Modflow2005Guide" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowNwtGuide" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowLgrGuide"
 rem "C:\Users\rbwinst\Documents\Embarcadero\Studio\Projects\ReplaceHttp\Win32\Debug\ReplaceHttp.exe" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\Modflow2000Guide" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowAllGuide" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowNwtGuide" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowOwhmGuide" "D:\Colab\GWModelTools\ModelMuse\Documentation5\ModflowGuide\ModflowLgrGuide"
@@ -45,10 +45,10 @@ copy Baggage\*.* ModflowNwtGuide\
 copy Baggage\*.* ModflowOwhmGuide\
 copy Baggage\*.* ModflowGuide\
 rem make zip files of chm help in webhelp directories
-"C:\Program Files\7-Zip\7z.exe" a WebModflow2000Guide\Modflow2000Guide.zip Modflow2000Guide\
-"C:\Program Files\7-Zip\7z.exe" a WebModflow2005Guide\Modflow2005Guide.zip Modflow2005Guide\
-"C:\Program Files\7-Zip\7z.exe" a WebModflowLgrGuide\ModflowLgrGuide.zip ModflowLgrGuide\
-"C:\Program Files\7-Zip\7z.exe" a WebModflowNwtGuide\ModflowNwtGuide.zip ModflowNwtGuide\
-"C:\Program Files\7-Zip\7z.exe" a WebModflowOwhmGuide\ModflowOwhmGuide.zip ModflowOwhmGuide\
-"C:\Program Files\7-Zip\7z.exe" a WebModflowGuide\ModflowGuide.zip ModflowGuide\
+"C:\Program Files\7-Zip\7z.exe" a WebModflow2000Guide\guide2000.zip Modflow2000Guide\
+"C:\Program Files\7-Zip\7z.exe" a WebModflow2005Guide\guide2005.zip Modflow2005Guide\
+"C:\Program Files\7-Zip\7z.exe" a WebModflowLgrGuide\GuideLGR.zip ModflowLgrGuide\
+"C:\Program Files\7-Zip\7z.exe" a WebModflowNwtGuide\guideNWT.zip ModflowNwtGuide\
+"C:\Program Files\7-Zip\7z.exe" a WebModflowOwhmGuide\guideOWHM.zip ModflowOwhmGuide\
+"C:\Program Files\7-Zip\7z.exe" a WebModflowGuide\guide.zip ModflowGuide\
 pause
