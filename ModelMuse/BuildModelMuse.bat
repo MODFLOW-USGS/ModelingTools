613fd4e4f23197e0ea077eeafe029516c51ec110
RichardWinston <rbwinston@mindspring.com>
2022-10-01 16:05:38 -0400

    Updated batch files to compile and build ModelMuse.

--- ModelMuse/BuildModelMuse.bat
+++ ModelMuse/BuildModelMuse.bat
@@ -2,5 +2,5 @@ rem Set the environment options using the Community Edition of Delphi version 10
 call "C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat"
 rem Build ModelMuse with current settings.
 MsBuild.exe /t:Build ModelMuse.dproj >Messages.txt
-Messages.txt
+start Messages.txt
 pause
