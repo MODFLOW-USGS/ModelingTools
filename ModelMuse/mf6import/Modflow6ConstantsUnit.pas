ac7db052d21ced733b40e390501ad9d18b1b431e
Richard Winston <rbwinst@usgs.gov>
2023-07-06 05:04:34 -0400

    Worked on importing MF6.

---
+++ ModelMuse/mf6import/Modflow6ConstantsUnit.pas
@@ -0,0 +1,66 @@
+unit Modflow6ConstantsUnit;
+
+interface
+
+uses
+  System.Math;
+
+const
+  // from Utilities\Constants.f90.
+  IUSERFORMATSTRIP = -99 ; //< default user format strip
+  IUSERFORMATWRAP = 99 ; //< default user format wrap
+  LENBIGLINE = 5000 ; //< maximum length of a big lineimplementation
+  LENHUGELINE = 50000 ; //< maximum length of a huge line
+  LENVARNAME = 16 ; //< maximum length of a variable nameend.
+  LENCOMPONENTNAME = 16 ; //< maximum length of a component name
+  LENCONTEXTNAME = 16 ; //< maximum length of a memory manager context
+  LENSOLUTIONNAME = LENCOMPONENTNAME ; //< maximum length of the solution name
+  LENMODELNAME = LENCOMPONENTNAME ; //< maximum length of the model name
+  LENPACKAGENAME = LENCOMPONENTNAME ; //< maximum length of the package name
+  LENEXCHANGENAME = LENCOMPONENTNAME ; //< maximum length of the exchange name
+  LENBUDROWLABEL = 2 * LENPACKAGENAME + 1 ; //< maximum length of the rowlabel string used in the budget table
+  LENMEMSEPARATOR = 1 ; //< maximum length of the memory path separator used, currently a '/'
+  LENMEMPATH =
+  LENCONTEXTNAME +
+  2 * LENCOMPONENTNAME +
+  2 * LENMEMSEPARATOR ; //< maximum length of the memory path
+  LENMEMADDRESS =
+  LENMEMPATH +
+  LENMEMSEPARATOR +
+  LENVARNAME ; //< maximum length of the full memory address, including variable name
+  LENAUXNAME = 16 ; //< maximum length of a aux variable
+  LENBOUNDNAME = 40 ; //< maximum length of a bound name
+  LENBUDTXT = 16 ; //< maximum length of a budget component names
+  LENPACKAGETYPE = 7 ; //< maximum length of a package type (DIS6, SFR6, CSUB6, etc.)
+  LENFTYPE = 5 ; //< maximum length of a package type (DIS, WEL, OC, etc.)
+  LENOBSNAME = 40 ; //< maximum length of a observation name
+  LENOBSTYPE = 30 ; //< maximum length of a observation type (CONTINUOUS)
+  LENTIMESERIESNAME = LENOBSNAME ; //< maximum length of a time series name
+  LENTIMESERIESTEXT = 16 ; //< maximum length of a time series text
+  LENDATETIME = 30 ; //< maximum length of a date time string
+  LINELENGTH = 300 ; //< maximum length of a standard line
+  LENLISTLABEL = 500 ; //< maximum length of a llist label
+  MAXCHARLEN = LENBIGLINE;
+  MAXOBSTYPES = 100 ; //< maximum number of observation types
+  NAMEDBOUNDFLAG = -9 ; //< named bound flag
+  LENPAKLOC = 34 ; //< maximum length of a package location
+  IZERO = 0 ; //< integer constant zero
+  IWETLAKE = 10000 ; //< integer constant for a dry lake
+  MAXADPIT = 100 ; //< maximum advanced package Newton-Raphson iterations
+  MAXMEMRANK = 3; //< maximum memory manager length (up to 3-dimensional arrays)
+  LENMEMTYPE = 50; //< maximum length of a memory manager type
+
+  LENERRMESSAGE = 1024; //< max length for the error message
+  LENGRIDTYPE = 16; //< max length for Fortran grid type string
+
+  // from srcbmi\mf6bmiUtil.f90
+  BMI_LENCOMPONENTNAME = 256; // max. length for the variable's address C-string
+  BMI_LENVARADDRESS = LENMEMADDRESS + 1; // component name length, i.e. 'MODFLOW 6'
+  BMI_LENERRMESSAGE = LENERRMESSAGE + 1; // max. length for the (exported) C-style error message
+  BMI_LENGRIDTYPE =  LENGRIDTYPE + 1; //< max. length for grid type C-strings
+  BMI_LENVARTYPE = LENMEMTYPE + 1; //< max. length for variable type C-strings
+  BMI_LENVERSION = 256; //< length of version string, e.g. '6.3.1' or '6.4.1-dev'
+
+implementation
+
+end.
--- ModelMuse/mf6import/hellomf6_D.dpr
+++ ModelMuse/mf6import/hellomf6_D.dpr
@@ -2,11 +2,16 @@ program HelloMf6;
 
 {$APPTYPE CONSOLE}
 
-uses madExcept, madLinkDisAsm, madListHardware, madListProcesses, madListModules,
-// {$LIBRARYPATH /c/Users/mjreno/Documents/dev/richard/winston}
-//{$LINKLIB libmf6d}
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  System.SysUtils,
+  Classes,
+  Modflow6ConstantsUnit in 'Modflow6ConstantsUnit.pas';
 
-  System.SysUtils, Classes;
 //  ctype
 
 type
@@ -23,28 +28,36 @@ type
   PcintArray = ^cintArray;
   TErrorMessage = Array[0..1024] of AnsiChar;
   PErrorMessage = ^TErrorMessage;
+  TGridType = Array[0..16] of AnsiChar;
 
 function initialize () : cint;cdecl;external 'libmf6.dll';
 function update () : cint;cdecl;external 'libmf6.dll';
 function finalize () : cint;cdecl;external 'libmf6.dll';
-function get_version (mf_version : PAnsiChar) : cint;cdecl;external 'libmf6.dll';
-function get_component_name(name : PAnsiChar) : cint;cdecl;external 'libmf6.dll';
+//function get_version (mf_version : PAnsiChar) : cint;cdecl;external 'libmf6.dll';
+function get_version (var mf_version : TMfName) : cint;cdecl;external 'libmf6.dll';
+//function get_component_name(name : PAnsiChar) : cint;cdecl;external 'libmf6.dll';
+function get_component_name(var name : TMfName) : cint;cdecl;external 'libmf6.dll';
 function get_output_item_count(count : pcint) : cint;cdecl;external 'libmf6.dll';
 
 // grid_id start at 1.
-function get_grid_type(grid_id : pcint; GridType: PAnsiChar) : cint;cdecl;external 'libmf6.dll';
-function get_grid_shape(grid_id : pcint; GridShape: PGridShape): cint;cdecl;external 'libmf6.dll';
+//function get_grid_type(grid_id : pcint; GridType: PAnsiChar) : cint;cdecl;external 'libmf6.dll';
+function get_grid_type(grid_id : pcint; var GridType: TGridType) : cint;cdecl;external 'libmf6.dll';
+
+function get_grid_shape(grid_id : pcint; var GridShape: TGridShape): cint;cdecl;external 'libmf6.dll';
 function get_output_var_names(Names: PAnsiChar): cint;cdecl;external 'libmf6.dll';
 function get_value_int(Variablename: PAnsiChar; AnArray: pcint): cint;cdecl;external 'libmf6.dll';
 function get_last_bmi_error(Error: PErrorMessage): cint;cdecl;external 'libmf6.dll';
-//function BMI_LENVARTYPE : cint;cdecl;external 'libmf6.dll';
+
+// Delphi does not support importing variables from DLLs or shared objects. :-(
+// var
+// BMI_LENVARTYPE : cint; cdecl;external 'libmf6.dll';
 
 var
   version : PAnsiChar;
-  vstr : array[0..255] of AnsiChar;
+  vstr : TMfName;
   component : PAnsiChar;
-  cname : array[0..255] of AnsiChar;
-  GridType : array[0..17] of AnsiChar;
+  cname : TMfName;
+  GridType : TGridType;
   count : pcint;
   cnt : cint;
   PGridType : PAnsiChar;
@@ -56,6 +69,7 @@ var
   NameIndex: Integer;
   NameBuilder: TStringBuilder;
   NameList: TStringList;
+  NameList2: TStringList;
   CharIndex: Integer;
   AChar: AnsiChar;
   AName: string;
@@ -64,6 +78,7 @@ var
   NperIDArray: cintArray;
 
   ErrorMessage: TErrorMessage;
+  NameStart: Integer;
 //  LenVarType: cint;
 begin
   version := @vstr;
@@ -71,8 +86,8 @@ begin
   PGridType := @GridType;
   writeln ('Hello, mf6 world.');
   initialize();
-  get_version(version);
-  get_component_name(component);
+  get_version(vstr);
+  get_component_name(cname);
   writeln('MF6 DLL version: ' + version);
   writeln('MF6 DLL component: ' + component);
   count := @cnt;
@@ -80,13 +95,14 @@ begin
   write('MF6 output item count: ');
   writeln(cnt);
 
-  SetLength(Names, (cnt+1)*256);
+  SetLength(Names, (cnt+1)*BMI_LENVARADDRESS);
 
   NamesP := @Names;
   get_output_var_names(Addr(Names[0]));
 
   NameBuilder := TStringBuilder.Create;
   NameList := TStringList.Create;
+  NameList2 := TStringList.Create;
   try
     for CharIndex := 0 to Length(Names) - 1 do
     begin
@@ -109,7 +125,16 @@ begin
     begin
       Writeln(NameList[NameIndex]);
     end;
+    for NameIndex := 0 to cnt - 1 do
+    begin
+      NameStart := NameIndex * BMI_LENVARADDRESS;
+      AName := string(PAnsiChar(Addr(Names[NameStart])));
+      NameList2.Add(AName);
+    end;
+    Assert(NameList.Count = NameList2.Count);
+    Assert(NameList.CommaText = NameList2.CommaText);
   finally
+    NameList2.Free;
     NameList.Free;
     NameBuilder.Free;
   end;
@@ -129,24 +154,35 @@ begin
   writeln('NPER: ', NperIDArray[0]);
 
   GridID := 1;
-  get_grid_type(@GridID, PGridType);
-  writeln('grid type: ' + GridType);
+  if get_grid_type(@GridID, GridType) = 0 then
+  begin
+    writeln('grid type: ' + GridType);
+  end
+  else
+  begin
+    writeln('Failure');
+  end;
 
   GridShape[0] := -1;
   GridShape[1] := -1;
   GridShape[2] := -1;
 
   GridShapeP := @GridShape;
-  get_grid_shape(@GridID, GridShapeP);
+  if get_grid_shape(@GridID, GridShape) = 0 then
 
 //  LenVarType := BMI_LENVARTYPE;
 //  WriteLn(' BMI_LENVARTYPE: ', LenVarType);
-
-  WriteLn('GridShape[0, NLAY]: ', GridShape[0]);
+  begin
+  WriteLn('GridShape[0], NLAY: ', GridShape[0]);
   WriteLn('GridShape[1], NROW: ', GridShape[1]);
   WriteLn('GridShape[2], NCOL: ', GridShape[2]);
+  end
+  else
+  begin
+    writeln('Failure');
+  end;
 
   update();
   finalize();
-  Readln;
+//  Readln;
 end.
