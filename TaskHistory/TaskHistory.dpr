894ad1c0ce1c07da5005467adba485844f879ced
rbwinst-usgs <rbwinst@usgs.gov>
2023-11-27 20:25:42 -0500

    Compiled Task History program.

--- TaskHistory/TaskHistory.dpr
+++ TaskHistory/TaskHistory.dpr
@@ -1,11 +1,6 @@
 program TaskHistory;
 
 uses
-  madExcept,
-  madLinkDisAsm,
-  madListHardware,
-  madListProcesses,
-  madListModules,
   Forms,
   MainUnit in 'MainUnit.pas' {frmTaskHistory},
   TaskHistoryUnit in 'TaskHistoryUnit.pas',
